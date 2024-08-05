217cefbc3f80d8f5a40c4e32d397bb14797231e5
gfirpipadro <107421729+gonzalofirpi@users.noreply.github.com>
2024-08-05 18:48:32 -0300

    probando animaciones

--- frontend/screens/Home.js
+++ frontend/screens/Home.js
@@ -1,20 +1,46 @@
-import { BlurView } from "expo-blur";
 import ScreenCenter from "../components/UI/ScreenCenter";
-import { Text, View } from "react-native";
+import Button from "../components/UI/Controls/Button";
+import { Colors } from "../constants/colors";
+import {
+  useAnimatedProps,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import SmileGlutty from "../components/UI/SVGGlutty/SmileGlutty";
 
 export default function Home() {
+  const y = useSharedValue(0);
+  const scaleY = useSharedValue(1);
+
+  const animatedProps = useAnimatedProps(() => {
+    return {
+      transform: [
+        { translateY: withSpring(y.value, { damping: 18 }) },
+        { scaleY: withSpring(scaleY.value, { damping: 18 }) },
+      ],
+    };
+  });
+
+  function greet() {
+    scaleY.value = -1;
+    y.value = 750;
+    setTimeout(() => {
+      y.value = 0;
+      scaleY.value = 1;
+    }, 2000);
+  }
+
   return (
     <ScreenCenter>
-      <Text>texto choto</Text>
-      <View style={{ height: 50, width: 50, backgroundColor: "red" }}></View>
-      <BlurView
-        intensity={10}
-        style={{
-          position: "absolute",
-          height: "100%",
-          width: "100%"
-        }}
+      <SmileGlutty
+        width={300}
+        height={300}
+        animatedLeftArm={animatedProps}
+        animatedRightArm={animatedProps}
       />
+      <Button backgroundColor={Colors.humita} onPress={greet}>
+        Levantar brazos
+      </Button>
     </ScreenCenter>
   );
 }
