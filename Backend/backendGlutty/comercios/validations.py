eddac4b628f40736be5a60a2bf744da6394df744
sofialmendraa <sofi.almendra02@gmail.com>
2024-08-26 18:23:36 -0300

    phone validation

--- Backend/backendGlutty/comercios/validations.py
+++ Backend/backendGlutty/comercios/validations.py
@@ -4,6 +4,6 @@ from django.core.exceptions import ValidationError
 def validate_phone(value):
     # if not value.isdigit():
     #     raise ValidationError("El número de teléfono debe contener solo dígitos.")
-    if len(value) != 13:  # Asumiendo que el teléfono debe tener 10 dígitos
-        raise ValidationError("El número de teléfono debe tener exactamente 10 dígitos.")
+    if len(value) > 14:  # Asumiendo que el teléfono debe tener 10 dígitos
+        raise ValidationError("El número de teléfono no debe tener más de 10 dígitos.")
     return value
