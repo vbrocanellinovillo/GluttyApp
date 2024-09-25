c96f6e099b768e3f3f200cad838c8673005a3c73
gfirpipadro <107421729+gonzalofirpi@users.noreply.github.com>
2024-09-25 18:20:48 -0300

    avanzando con el wheel picker para la variable

--- frontend/components/UI/Controls/Picker.js
+++ frontend/components/UI/Controls/Picker.js
@@ -1,24 +1,88 @@
 import WheelPicker from "@quidone/react-native-wheel-picker";
 import { useState } from "react";
-import { Pressable } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import TextCommonsRegular from "../FontsTexts/TextCommonsRegular";
 import BlurContainer from "../BlurContainer";
+import { Colors } from "../../../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import * as Haptics from "expo-haptics";
 
 export default function Picker({ data, value }) {
   const [visible, setVisible] = useState(false);
 
   function toggleVisible() {
+    Haptics.selectionAsync();
     setVisible((prevVisible) => !prevVisible);
   }
 
   return (
     <>
-      <Pressable onPress={toggleVisible}>
-        <TextCommonsRegular>holis</TextCommonsRegular>
+      <Pressable
+        onPress={toggleVisible}
+        style={({ pressed }) =>
+          pressed ? [styles.container, styles.pressed] : [styles.container]
+        }
+      >
+        <TextCommonsRegular style={styles.valueText}>value</TextCommonsRegular>
+        <Ionicons name="chevron-down" color={Colors.mJordan} size={18} />
       </Pressable>
-      <BlurContainer visible={visible} onDismiss={toggleVisible}>
-        <WheelPicker data={data} />
+      <BlurContainer
+        visible={visible}
+        onDismiss={toggleVisible}
+        backdropContainerStyle={styles.blurredContainer}
+        backdropStyle={styles.blurredBackdrop}
+      >
+        <WheelPicker
+          data={data}
+          style={styles.wheelPicker}
+          onValueChanging={() =>
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+          }
+        />
       </BlurContainer>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#eee",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    padding: 14,
+    width: "100%",
+    borderRadius: 12,
+  },
+
+  pressed: {
+    opacity: 0.7,
+  },
+
+  valueText: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: Colors.mJordan,
+  },
+
+  blurredContainer: {
+    justifyContent: "center",
+  },
+
+  blurredBackdrop: {
+    backgroundColor: "rgba(0,0,0,0.3)",
+  },
+
+  wheelPicker: {
+    backgroundColor: "#ddd",
+    borderRadius: 12,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.8,
+    shadowRadius: 10,
+  },
+});
