e136b30b2b813da0892eb5f85761e3c487f6ff16
Francisco <franm2910@gmail.com>
2024-09-30 20:13:51 -0300

    A margarita la quiero YO!
    Eliminar estudio medico front listo

---
+++ frontend/components/UI/contextualMenu.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Colors } from "../../constants/colors";
+
+export default function ContextualMenu({ onEdit, onDelete }) {
+  return (
+    <View style={styles.menuContainer}>
+      {/* Botón para eliminar */}
+      <TouchableOpacity style={styles.menuItem} onPress={onDelete}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          size={24}
+          color={Colors.darkGray}
+          style={styles.iconStyle}
+        />
+        <Text style={styles.menuText}>Eliminar Estudio</Text>
+      </TouchableOpacity>
+
+      {/* Botón para editar */}
+      <TouchableOpacity style={styles.menuItem} onPress={onEdit}>
+        <MaterialCommunityIcons
+          name="pencil-outline"
+          size={24}
+          color={Colors.darkGray}
+          style={styles.iconStyle}
+        />
+        <Text style={styles.menuText}>Editar Estudio</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  menuContainer: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 10,
+    shadowColor: "rgba(0,0,0,0.2)",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 4,
+    elevation: 5,
+    position: "absolute", // Para posicionarlo donde necesites
+    top: 50, // Cambia según dónde quieres que aparezca
+    right: 10,
+    zIndex: 10,
+  },
+  menuItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  iconStyle: {
+    marginRight: 10,
+  },
+  menuText: {
+    fontSize: 16,
+    color: Colors.darkGray,
+  },
+});
--- frontend/screens/User/MedicalExams/ViewMedicalExam.js
+++ frontend/screens/User/MedicalExams/ViewMedicalExam.js
@@ -1,9 +1,9 @@
-import { Text, View, StyleSheet, ScrollView } from "react-native";
+import React, { useState, useCallback } from "react";
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import { useSelector } from "react-redux";
-import { useState, useCallback } from "react";
-import { Colors } from "../../../constants/colors";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Colors } from "../../../constants/colors";
 import TextCommonsMedium from "../../../components/UI/FontsTexts/TextCommonsMedium";
 import TextCommonsRegular from "../../../components/UI/FontsTexts/TextCommonsRegular";
 import MedicalExamDateViewer from "../../../components/MedicalExams/MedicalExamDateViewer";
@@ -11,15 +11,19 @@ import Button from "../../../components/UI/Controls/Button";
 import FormSectionContainer from "../../../components/UI/Forms/FormSectionContainer";
 import RangeBar from "../../../components/MedicalExams/RangeBar";
 import ValorPosNeg from "../../../components/MedicalExams/ValorPosNeg";
-import { getMedicalExamById } from "../../../services/medicalExamService";
+import { deleteMedicalExam, getMedicalExamById } from "../../../services/medicalExamService";
 import LoadingGlutty from "../../../components/UI/Loading/LoadingGlutty";
-import AddButton from "../../../components/UI/Controls/AddButton";
+import ContextualMenu from "../../../components/UI/contextualMenu";
+import GluttyModal from "../../../components/UI/GluttyModal";
+import MedicalStatistics from "./MedicalStatistics";
 
-
-export default function ViewMedicalExam({ route }) {
+export default function ViewMedicalExam({ navigation, route }) {
   const [medicalExam, setMedicalExam] = useState(undefined);
   const [isloading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [showMenu, setShowMenu] = useState(false); // Estado para mostrar el menú contextual
+  const [showModal, setShowModal] = useState(false);
+  const [message, setMessage] = useState("");
 
   const id = route.params.id;
   console.log(id);
@@ -28,103 +32,147 @@ export default function ViewMedicalExam({ route }) {
   console.log(token);
   
   useFocusEffect(
-     useCallback(() => {
-        const cargarEstudioMedico = async () => {
-     try {
+    useCallback(() => {
+      const cargarEstudioMedico = async () => {
+        try {
           setIsLoading(true);
           const medicExam = await getMedicalExamById(id, token);
-          console.log("Medico", medicExam)
+          console.log("Medico", medicExam);
           setMedicalExam(medicExam);
-       } catch (error) {
+        } catch (error) {
           setIsError(true);
-       } finally {
-         setIsLoading(false);
-       }
-     };
+        } finally {
+          setIsLoading(false);
+        }
+      };
 
-    if (id && token) {
-       cargarEstudioMedico();
-    }
-  }, [id, token])
+      if (id && token) {
+        cargarEstudioMedico();
+      }
+    }, [id, token])
   );
 
- return ( 
-  <>
-    {/* Muestra el loading mientras se cargan los datos */}
-    <LoadingGlutty visible={isloading} color={Colors.vainilla} />
+  async function handleDelete () {
+    // Eliminar el estudio
+    console.log("Eliminar Estudio");
+    try {
+      setIsLoading(true);
+      console.log("Eliminando estudio");
+      console.log("whastt: ", id)
+      const response = await deleteMedicalExam(id, token);
+      console.log("La respuestaaa:")
+      console.log(response)
+      setMessage(response.detail);
+      setShowModal(true);
+      navigation.navigate("MedicalStatistics")
+    } catch (error) {
+      setIsError(true);
+      setMessage(error.message);
+      setShowModal(true);
+    } finally {
+      setIsLoading(false);
+    }
+    
+  };
 
-    {/* Verifica si ya hay datos cargados antes de renderizar el contenido */}
-    { medicalExam && (
-      <>
-        {/* ENCABEZADO */}
-        <View style={styles.container}>
-          <View style={styles.dateBox}>
-            <MedicalExamDateViewer date={medicalExam.date} />
-          </View>
+  const handleEdit = () => {
+    // Aquí iría tu lógica para editar el estudio
+    console.log("Editar Estudio");
+  };
 
-          <View style={styles.infoBox}>
-            <TextCommonsMedium style={styles.examTitle}>
-              Análisis de Sangre
-            </TextCommonsMedium>
+  function closeModalHandler() {
+    setShowModal(false);
+    if (!isError) {
+      dispatch(commerceActions.updateBranch({branch}))
+      navigation.navigate("CommerceDrawer");
+    }
+  }
 
-            <View style={styles.locationBox}>
-              <MaterialCommunityIcons
-                name="map-marker"
-                size={24} descriptive
-                color={Colors.mJordan}
-              />
-              
-            <TextCommonsRegular style={styles.locationText}>
-              {medicalExam.lab}
-            </TextCommonsRegular>
-              
+  return (
+    <>
+      <GluttyModal
+        isError={isError}
+        message={message}
+        onClose={closeModalHandler}
+        visible={showModal}
+      />
+      {/* Muestra el loading mientras se cargan los datos */}
+      <LoadingGlutty visible={isloading} color={Colors.vainilla} />
+
+      {/* Verifica si ya hay datos cargados antes de renderizar el contenido */}
+      {medicalExam && (
+        <>
+          {/* ENCABEZADO */}
+          <View style={styles.container}>
+            <View style={styles.dateBox}>
+              <MedicalExamDateViewer date={medicalExam.date} />
+            </View>
+
+            <View style={styles.infoBox}>
+              <TextCommonsMedium style={styles.examTitle}>
+                Análisis de Sangre
+              </TextCommonsMedium>
+
+              <View style={styles.locationBox}>
+                <MaterialCommunityIcons
+                  name="map-marker"
+                  size={24}
+                  color={Colors.mJordan}
+                />
+                <TextCommonsRegular style={styles.locationText}>
+                  {medicalExam.lab}
+                </TextCommonsRegular>
+              </View>
+
+              {/* Botón de menú contextual (tres puntos) */}
+              <TouchableOpacity onPress={() => setShowMenu(!showMenu)}>
+                <MaterialCommunityIcons name="dots-vertical" size={24} color={Colors.darkGray} />
+              </TouchableOpacity>
             </View>
           </View>
-        </View>
 
-        {/* PDF */}
-        <View style={{ padding: 20, flexDirection:"row", justifyContent: "space-between", marginHorizontal: 10}}>
-          <Button style={styles.opciones} backgroundColor={Colors.locro}>Ver PDF</Button>
-          <Button style={styles.opciones} backgroundColor={Colors.locro}>Eliminar</Button>
-          <Button style={styles.opciones} backgroundColor={Colors.locro}>Editar</Button>
-        </View>
+          {/* Renderiza el menú contextual */}
+          {showMenu && <ContextualMenu onEdit={handleEdit} onDelete={handleDelete} />}
 
-        {/* VARIABLES*/}
+          {/* PDF */}
+          <View style={{ padding: 20, flexDirection:"row", justifyContent: "space-between", marginHorizontal: 10 }}>
+            <Button style={styles.opciones} backgroundColor={Colors.locro}>Ver PDF</Button>
+          </View>
 
-        <TextCommonsMedium style={styles.titleVarMed}>Variables Médicas</TextCommonsMedium>
-        <ScrollView style={styles.scrollview}>
-        
-        {medicalExam.variables.map((item) => (
-        <View key={item.variable_name}>
-        {(item.value === "Positivo" || item.value === "Negativo") ? (
-          <FormSectionContainer>
-            <ValorPosNeg 
-              label={item.variable_name} 
-              valor={item.value} 
-              descrip={item.description} 
-            />
-          </FormSectionContainer>
-        ) : (
-          <FormSectionContainer>
-            <RangeBar 
-              label={item.variable_name} 
-              minBarraGris={item.min_value} 
-              maxBarraGris={item.max_value}  
-              normalMin={item.min_value} 
-              normalMax={item.max_value} 
-              currentValue={item.value} 
-              descrip={item.description} 
-              lab={medicalExam.lab} 
-            />
-          </FormSectionContainer>
-        )}
-      </View>))}
-       </ScrollView>
-      </>
-    )}
-  </>
-);
-
+          {/* VARIABLES */}
+          <TextCommonsMedium style={styles.titleVarMed}>Variables Médicas</TextCommonsMedium>
+          <ScrollView style={styles.scrollview}>
+            {medicalExam.variables.map((item) => (
+              <View key={item.variable_name}>
+                {(item.value === "Positivo" || item.value === "Negativo") ? (
+                  <FormSectionContainer>
+                    <ValorPosNeg 
+                      label={item.variable_name} 
+                      valor={item.value} 
+                      descrip={item.description} 
+                    />
+                  </FormSectionContainer>
+                ) : (
+                  <FormSectionContainer>
+                    <RangeBar 
+                      label={item.variable_name} 
+                      minBarraGris={item.min_value} 
+                      maxBarraGris={item.max_value}  
+                      normalMin={item.min_value} 
+                      normalMax={item.max_value} 
+                      currentValue={item.value} 
+                      descrip={item.description} 
+                      lab={medicalExam.lab} 
+                    />
+                  </FormSectionContainer>
+                )}
+              </View>
+            ))}
+          </ScrollView>
+        </>
+      )}
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -167,17 +215,14 @@ const styles = StyleSheet.create({
     color: Colors.darkGray, // Elige un color de texto adecuado
   },
   scrollview: {
-    //marginBottom: 20,
     paddingBottom: 200,
     marginBottom: 110,
   },
-  titleVarMed:{
+  titleVarMed: {
     fontSize: 24,
     fontWeight: "bold",
     color: Colors.locro,
     marginLeft: 20,
-   },
-   opciones:{
-   },
+  },
+  opciones: {},
 });
-
