5f837027f351449ed82dd2a90b1cb99c3305b288
delfidionisio <delfidionisio19@gmail.com>
2024-10-11 19:30:39 -0300

    Casi listo el add post

--- frontend/screens/Community/AddPost.js
+++ frontend/screens/Community/AddPost.js
@@ -1,27 +1,42 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, TextInput, TouchableOpacity, StyleSheet, Keyboard, Animated, TouchableWithoutFeedback, ScrollView, Text } from 'react-native';
+import { View, TextInput, TouchableOpacity, StyleSheet, Keyboard, Animated, TouchableWithoutFeedback, ScrollView, Text, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import TextCommonsMedium from '../../components/UI/FontsTexts/TextCommonsMedium';
 import { Colors } from '../../constants/colors';
+import BottomSheet from "@devvie/bottom-sheet";
+import ImageSheetOptions from "../../components/UI/UserImage/ImageSheetOptions";
+import {
+  launchCameraAsync,
+  launchImageLibraryAsync,
+  MediaTypeOptions,
+  PermissionStatus,
+  useCameraPermissions,
+  useMediaLibraryPermissions,
+} from "expo-image-picker";
 
 export default function AddPost() {
   const [post, setPost] = useState('');
   const [tags, setTags] = useState([]);
   const [currentTag, setCurrentTag] = useState('');
+  const [selectedImages, setSelectedImages] = useState([]); 
 
   const keyboardHeight = useRef(new Animated.Value(0)).current;
   const inputTranslateY = useRef(new Animated.Value(0)).current;
+
+  const sheetRef = useRef();
+  const [cameraPermissions, requestCameraPermissions] = useCameraPermissions();
+  const [galleryPermissions, requestGalleryPermissions] = useMediaLibraryPermissions();
 
   useEffect(() => {
     const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', (event) => {
       Animated.parallel([
         Animated.timing(keyboardHeight, {
-          toValue: 250, //event.endCoordinates.height, 
+          toValue: 250,
           duration: 150,
           useNativeDriver: false,
         }),
         Animated.timing(inputTranslateY, {
-          toValue: -250,//-event.endCoordinates.height,
+          toValue: -250,
           duration: 150,
           useNativeDriver: false,
         }),
@@ -49,9 +64,8 @@ export default function AddPost() {
     };
   }, []);
 
-  // Manejar la entrada de etiquetas
   const handleTagInput = (text) => {
-    if (text.includes(' ')) {
+    if (text.includes(' ')) { 
       if (currentTag.trim()) {
         setTags([...tags, currentTag.trim()]);
       }
@@ -61,14 +75,63 @@ export default function AddPost() {
     }
   };
 
-  // Función para eliminar una etiqueta por su índice
   const removeTag = (index) => {
     setTags(tags.filter((_, i) => i !== index));
+  };
+
+  const removeImage = (index) => {
+    setSelectedImages(selectedImages.filter((_, i) => i !== index));
   };
 
   const dismissKeyboard = () => {
     Keyboard.dismiss();
   };
+
+  // Función para abrir la hoja con las opciones de imagen
+  function openImageOptions() {
+    sheetRef.current?.open();
+  }
+
+  // Función para comprobar permisos y seleccionar imágenes
+  async function selectImage(option) {
+    sheetRef.current?.close();
+    const permissionStatus = await checkPermissions(option);
+
+    if (!permissionStatus) return;
+
+    let imageResult;
+    const options = {
+      allowsEditing: true,
+      mediaTypes: MediaTypeOptions.Images,
+    };
+
+    if (option === "Take Image") {
+      imageResult = await launchCameraAsync(options);
+    } else {
+      imageResult = await launchImageLibraryAsync(options);
+    }
+
+    if (!imageResult.canceled) {
+      setSelectedImages([...selectedImages, imageResult.assets[0].uri]);
+    }
+  }
+
+  // Verifica permisos de cámara o galería
+  async function checkPermissions(option) {
+    if (option === "Take Image") {
+      if (cameraPermissions.status === PermissionStatus.GRANTED) {
+        return true;
+      }
+      const response = await requestCameraPermissions();
+      return response.granted;
+    } else {
+      if (galleryPermissions.status === PermissionStatus.GRANTED) {
+        return true;
+      }
+      const response = await requestGalleryPermissions();
+      return response.granted;
+    }
+  }
 
   return (
     <TouchableWithoutFeedback onPress={dismissKeyboard}>
@@ -83,29 +146,37 @@ export default function AddPost() {
           multiline={true}
         />
 
-        {/* Animación para las etiquetas y el input */}
+        {/* Mostrar las imágenes seleccionadas */}
+        <ScrollView horizontal style={styles.imagesContainer}>
+          {selectedImages.map((imageUri, index) => (
+            <View key={index} style={styles.imageWrapper}>
+              <Image
+                source={{ uri: imageUri }}
+                style={styles.imagePreview}
+              />
+              <TouchableOpacity
+                style={styles.removeIcon}
+                onPress={() => removeImage(index)}
+              >
+                <Ionicons name="trash" size={20} color="black" />
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+
         <Animated.View style={{ transform: [{ translateY: inputTranslateY }] }}>
-          {/* Etiqueta para las etiquetas */}
           <TextCommonsMedium style={styles.etiquetasLabel}>Etiquetas de búsqueda</TextCommonsMedium>
 
-          {/* Input de etiquetas */}
           <TextInput
             style={styles.inputEtiqueta}
             placeholder="#Córdoba  #GeneralPaz  #Vegano "
             placeholderTextColor="#999"
             value={currentTag}
             onChangeText={handleTagInput}
-            onSubmitEditing={() => {
-              if (currentTag.trim()) {
-                setTags([...tags, currentTag.trim()]);
-                setCurrentTag('');
-              }
-            }}
             multiline={true}
             maxLength={130}
           />
 
-          {/* Mostrar etiquetas */}
           <ScrollView style={styles.tagsContainer} horizontal>
             {tags.map((tag, index) => (
               <View key={index} style={styles.tag}>
@@ -118,18 +189,22 @@ export default function AddPost() {
           </ScrollView>
         </Animated.View>
 
-        {/* Botones animados (cámara y enviar) */}
         <Animated.View style={[styles.buttonsContainer, { bottom: keyboardHeight }]}>
-          {/* Botón de la cámara */}
-          <TouchableOpacity style={styles.photoButton}>
+          <TouchableOpacity style={styles.photoButton} onPress={openImageOptions}>
             <Ionicons name="camera-outline" size={24} color="white" />
           </TouchableOpacity>
 
-          {/* Botón de enviar */}
           <TouchableOpacity style={styles.sendButton}>
             <Ionicons name="send" size={24} color="white" />
           </TouchableOpacity>
         </Animated.View>
+
+        <BottomSheet ref={sheetRef} height={200}>
+          <ImageSheetOptions
+            onTakeImage={selectImage.bind(this, "Take Image")}
+            onPickImage={selectImage.bind(this, "Pick Image")}
+          />
+        </BottomSheet>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -172,7 +247,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     marginTop: 10,
     maxWidth: 280,
-    //height: 150,
+    marginBottom: 110,
   },
   tag: {
     flexDirection: 'row',
@@ -215,10 +290,27 @@ const styles = StyleSheet.create({
     marginBottom: 130,
   },
   etiquetasLabel: {
-    marginTop: 280,
     marginBottom: 10,
     color: Colors.locro,
     fontWeight: 'bold',
     fontSize: 18,
+  },
+  imageWrapper: {
+    position: 'relative',
+    marginRight: 10,
+  },
+  imagePreview: {
+    width: 120,
+    height: 120,
+    borderRadius: 10,
+    marginHorizontal: 3,
+  },
+  removeIcon: {
+    position: 'absolute',
+    top: 5,
+    right: 16,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 2,
   },
 });
