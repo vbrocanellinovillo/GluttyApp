56a1a6a1270875d715272c6e94084e891be606df
gfirpipadro <107421729+gonzalofirpi@users.noreply.github.com>
2024-10-12 20:36:26 -0300

    cambio temporal de bd

--- Backend/backendGlutty/backendGlutty/settings.py
+++ Backend/backendGlutty/backendGlutty/settings.py
@@ -15,6 +15,7 @@ from datetime import timedelta
 import os
 from dotenv import load_dotenv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,82 +24,80 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qqie=%4sczzc)!fap#yh!pogqcad!pquwj+=-l6&zcleax3#rf'
+SECRET_KEY = "django-insecure-qqie=%4sczzc)!fap#yh!pogqcad!pquwj+=-l6&zcleax3#rf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # 'django-extensions',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'rest_framework_simplejwt',
-    'corsheaders',
-    'usuarios',
-    'productos',
-    'comercios',
-    'estudios',
+    "rest_framework",
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
+    "corsheaders",
+    "usuarios",
+    "productos",
+    "comercios",
+    "estudios",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'usuarios.middleware.JWTUserMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "usuarios.middleware.JWTUserMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'backendGlutty.urls'
+ROOT_URLCONF = "backendGlutty.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'backendGlutty.wsgi.application'
+WSGI_APPLICATION = "backendGlutty.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'celinahunziker',
-        'USER': 'celinahunziker',
-        'PASSWORD': 'niHosDyld/ov4nus60',
-        'HOST': '200.69.137.167',
-        'PORT': '54321',
-        'CONN_MAX_AGE': 600,  # Reutiliza conexiones hasta por 10 minutos
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+        "CONN_MAX_AGE": 600,  # Reutiliza conexiones hasta por 10 minutos
     }
 }
-
 
 
 # Password validation
@@ -114,41 +113,39 @@ DATABASES = {
 # }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': SECRET_KEY,
-    'VERIFYING_KEY': None,
-    'AUTH_HEADER_TYPES': ('Bearer',),
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
-    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -157,26 +154,26 @@ load_dotenv()
 
 # Configuration cloudinary
 CLOUDINARY = {
-    'cloud_name': os.getenv('CLOUD_NAME'),
-    'api_key': os.getenv('API_KEY_CLOUDINARY'),
-    'api_secret': os.getenv('API_SECRET_CLOUDINARY'),
+    "cloud_name": os.getenv("CLOUD_NAME"),
+    "api_key": os.getenv("API_KEY_CLOUDINARY"),
+    "api_secret": os.getenv("API_SECRET_CLOUDINARY"),
 }
 
 # Emails
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'America/Argentina/Cordoba'
+TIME_ZONE = "America/Argentina/Cordoba"
 
 USE_I18N = True
 
@@ -188,13 +185,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-AUTH_USER_MODEL = 'usuarios.User'
+AUTH_USER_MODEL = "usuarios.User"
--- Backend/backendGlutty/cargaTipoProducto.py
+++ Backend/backendGlutty/cargaTipoProducto.py
@@ -2,21 +2,27 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # Leer el archivo Excel
-df = pd.read_excel('C:/Users/celih/Downloads/productosANMAT.xlsx')
+df = pd.read_excel(
+    "C:\\Users\\Usuario\\Downloads\\busqueda-listado-alg-12-10-2024.xlsx"
+)
 
 # Obtener los valores únicos de la columna 'TipoProducto'
-tipos_productos_unicos = df['TipoProducto'].unique()
+tipos_productos_unicos = df["TipoProducto"].unique()
 print(tipos_productos_unicos)
 
 # # Convertir a un DataFrame, ordenar alfabéticamente y eliminar duplicados
-df_tipos_productos = pd.DataFrame(tipos_productos_unicos, columns=['nombre'])
-df_tipos_productos.sort_values(by='nombre', inplace=True)
+df_tipos_productos = pd.DataFrame(tipos_productos_unicos, columns=["nombre"])
+df_tipos_productos.sort_values(by="nombre", inplace=True)
 print(df_tipos_productos)
 
 # # Conectar a la base de datos PostgreSQL
-engine = create_engine('postgresql+psycopg2://celinahunziker:niHosDyld/ov4nus60@200.69.137.167:54321/postgres')
+engine = create_engine(
+    "postgresql://postgres.bdmsnvgoqjpihjjmcwxg:xOVcMk9x7mQz70HU@aws-0-sa-east-1.pooler.supabase.com:6543/postgres"
+)
 
 # # Insertar los valores únicos y ordenados en la tabla 'tipos_producto'
-df_tipos_productos.to_sql('productos_tipoproducto', engine, if_exists='append', index=False)
+df_tipos_productos.to_sql(
+    "productos_tipoproducto", engine, if_exists="append", index=False
+)
 
 # print("Tipos de productos insertados correctamente.")
