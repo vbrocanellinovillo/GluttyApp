528fbb73f406dfc5e5538fc285855aa688a91c99
gfirpipadro <107421729+gonzalofirpi@users.noreply.github.com>
2024-10-25 19:19:42 -0300

    corregi el estilo del ver mas imagenes

--- frontend/components/Community/ImagesContainer.js
+++ frontend/components/Community/ImagesContainer.js
@@ -1,6 +1,8 @@
-import { View, Image, StyleSheet, Text } from "react-native";
+import { View, Image, StyleSheet, Text, Pressable } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { postBackgroundColor } from "../../constants/community";
+import { Colors } from "../../constants/colors";
+import TextCommonsMedium from "../UI/FontsTexts/TextCommonsMedium";
 
 const IMAGE_HEIGHT_ONLY_ROW = 284;
 const IMAGE_HEIGHT_TWO_ROW = 140;
@@ -10,56 +12,60 @@ export default function ImagesContainer({ images = [] }) {
   const imagesCount = images.length;
 
   return (
-    <View style={[styles.container, { gap: imagesCount === 3 && 2 }]}>
-      {imagesCount === 1 && (
-        <Image source={{ uri: images[0] }} style={styles.singleImage} />
-      )}
-      {(imagesCount === 2 || imagesCount === 4) &&
-        images.map((image, index) => (
-          <Image
-            key={index}
-            source={{ uri: image }}
-            style={[
-              styles.equalImage,
-              {
-                height:
-                  imagesCount === 2
-                    ? IMAGE_HEIGHT_ONLY_ROW
-                    : IMAGE_HEIGHT_TWO_ROW,
-              },
-            ]}
-          />
-        ))}
-      {imagesCount === 3 && (
-        <>
-          <Image source={{ uri: images[0] }} style={styles.largeImage} />
-          <View style={styles.smallImagesContainer}>
-            {images.slice(1, 3).map((image, index) => (
-              <Image
-                key={index}
-                source={{ uri: image }}
-                style={styles.smallImage}
-              />
-            ))}
-          </View>
-        </>
-      )}
-      {imagesCount > 4 && (
-        <>
-          {images.slice(0, 3).map((image, index) => (
+    <Pressable>
+      <View style={[styles.container, { gap: imagesCount === 3 && 2 }]}>
+        {imagesCount === 1 && (
+          <Image source={{ uri: images[0] }} style={styles.singleImage} />
+        )}
+        {(imagesCount === 2 || imagesCount === 4) &&
+          images.map((image, index) => (
             <Image
               key={index}
               source={{ uri: image }}
-              style={[styles.equalImage, { height: IMAGE_HEIGHT_TWO_ROW }]}
+              style={[
+                styles.equalImage,
+                {
+                  height:
+                    imagesCount === 2
+                      ? IMAGE_HEIGHT_ONLY_ROW
+                      : IMAGE_HEIGHT_TWO_ROW,
+                },
+              ]}
             />
           ))}
-          <View style={styles.plusContainer}>
-            <Ionicons name="eye" size={24} color="white" />
-            <Text style={styles.moreText}>+{imagesCount - 3}</Text>
-          </View>
-        </>
-      )}
-    </View>
+        {imagesCount === 3 && (
+          <>
+            <Image source={{ uri: images[0] }} style={styles.largeImage} />
+            <View style={styles.smallImagesContainer}>
+              {images.slice(1, 3).map((image, index) => (
+                <Image
+                  key={index}
+                  source={{ uri: image }}
+                  style={styles.smallImage}
+                />
+              ))}
+            </View>
+          </>
+        )}
+        {imagesCount > 4 && (
+          <>
+            {images.slice(0, 3).map((image, index) => (
+              <Image
+                key={index}
+                source={{ uri: image }}
+                style={[styles.equalImage, { height: IMAGE_HEIGHT_TWO_ROW }]}
+              />
+            ))}
+            <View style={styles.plusContainer}>
+              <Ionicons name="eye" size={26} color={Colors.lightOcean} />
+              <TextCommonsMedium style={styles.moreText}>
+                +{imagesCount - 3}
+              </TextCommonsMedium>
+            </View>
+          </>
+        )}
+      </View>
+    </Pressable>
   );
 }
 
@@ -105,6 +111,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     width: "50%",
+    backgroundColor: Colors.oceanBlue,
     borderWidth: IMAGES_GAP,
+    borderColor: postBackgroundColor,
+  },
+
+  moreText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: Colors.lightOcean,
   },
 });
