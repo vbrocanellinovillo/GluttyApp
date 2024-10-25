2b7376384a713eca3d1ffd8067ae6228f892a6aa
sofialmendraa <sofi.almendra02@gmail.com>
2024-10-25 18:57:12 -0300

    cambio de nombres de endpoints y funciones

--- Backend/backendGlutty/recetas/urls.py
+++ Backend/backendGlutty/recetas/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("generate-recipe/", generate_recipe, name="generate_recipe"),
-    path("add-favorite/", add_favorite, name="add_favorite"),
-    path("get-favorite/", get_favorite_by_id, name="get_favorite_by_id"),
-    path("get-favorites/", get_favorites, name="get_favorites"),
+    path("save-message/", save_message, name="add_favorite"),
+    path("get-message/", get_saved_message_by_id, name="get_saved_message_by_id"),
+    path("get-all-messages/", get_saved_messages, name="get_saved_messages"),
 ]
