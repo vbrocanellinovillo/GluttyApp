c07809fdb1df4f419cec0504b93ce7526c508b49
celhunziker <celihunziker@gmail.com>
2024-10-26 18:24:16 -0300

    agrego id de chatbot a bd, genero toggle favorite message y modifico el diccionario de malas palabras

--- Backend/backendGlutty/recetas/urls.py
+++ Backend/backendGlutty/recetas/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("ask-chatbot/", ask_chatbot, name="ask_chatbot"),
-    path("save-message/", save_message, name="add_favorite"),
+    path("toggle-save-message/", toggle_save_message, name="add_favorite"),
     path("get-message/", get_saved_message_by_id, name="get_saved_message_by_id"),
     path("get-all-messages/", get_saved_messages, name="get_saved_messages"),
 ]
