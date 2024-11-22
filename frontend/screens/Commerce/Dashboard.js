6c7d068f06e29a651a378cf80940d70b32fc781c
delfidionisio <delfidionisio19@gmail.com>
2024-11-22 16:36:09 -0300

    dashboard 4

--- frontend/screens/Commerce/Dashboard.js
+++ frontend/screens/Commerce/Dashboard.js
@@ -1,12 +1,23 @@
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet } from "react-native"
 import BoxDashboard from "../../components/Dashboard/boxDashboard"
 import { comment, heart, star } from "../../constants/imageIcons"
 
 
 export function Dashboard(){
     return (
-        <View>
+        <View style={styles.container}>
             <BoxDashboard image={heart} number={120}></BoxDashboard>
+            <BoxDashboard image={star} number={80}></BoxDashboard>
+            <BoxDashboard image={comment} number={45}></BoxDashboard>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row", 
+        justifyContent: "space-around", 
+        alignItems: "center", 
+        padding: 16, 
+    }
+})
