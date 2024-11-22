1ce9539ee697d868f592e1e0693f33d2387562dc
delfidionisio <delfidionisio19@gmail.com>
2024-11-22 16:28:41 -0300

    dashboard 3

---
+++ frontend/constants/imageIcons.js
@@ -0,0 +1,8 @@
+export const star =
+  "https://res.cloudinary.com/dksmkvi49/image/upload/v1732303279/estrella2x_hxxqf9.webp";
+
+export const heart =
+  "https://res.cloudinary.com/dksmkvi49/image/upload/v1732303305/corazonn2x_yd2ftt.webp";
+
+export const comment =
+  "https://res.cloudinary.com/dksmkvi49/image/upload/v1732303343/comment2x_j3yu2n.webp";
--- frontend/screens/Commerce/Dashboard.js
+++ frontend/screens/Commerce/Dashboard.js
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native"
+import BoxDashboard from "../../components/Dashboard/boxDashboard"
+import { comment, heart, star } from "../../constants/imageIcons"
 
 
 export function Dashboard(){
     return (
         <View>
-            <Text>Dashboard Screen</Text>
+            <BoxDashboard image={heart} number={120}></BoxDashboard>
         </View>
     )
 }
