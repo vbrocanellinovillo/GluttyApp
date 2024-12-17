dcc892da9e590cca0c3e7f2dc885fd78fc28bde5
delfidionisio <delfidionisio19@gmail.com>
2024-12-17 00:18:24 -0300

    Update MenusContainer.js

--- frontend/components/Map/MenusContainer.js
+++ frontend/components/Map/MenusContainer.js
@@ -6,7 +6,7 @@ import { Colors } from "../../constants/colors";
 export default function MenusContainer({ menus = [], handlePdf }) {
   return (
     <ElementsContainer
-      title="Menus"
+      title="Menú"
       items={menus}
       scrollContainerStyle={styles.container}
     >
